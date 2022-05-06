514b06dfa24366ed9925451ae40a7ac0ed904b45
unknown <Caio>
2022-05-06 16:26:34 -0300

    Aula 4 - Parte 2 - Criação do Rodapé e ajustes na navegação

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 function onScroll() {
-  navigation.classList.add('scroll')
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
 
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
+
+function showBackToTopButtonOnScroll() {
+  /*console.log(scrollY)*/
+  if (scrollY > 1200) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+
+  backToTopButton.classList.toggle('inverseButton', scrollY > 5078)
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
