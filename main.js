cb80fafadf405bd01810eb32da37680a36b315f6
Caio <caiogoncalves101@gmail.com>
2022-05-08 22:36:20 -0300

    Aula 5 - Finalização - Criação da responsavidade Desktop. Alterações no CSS/JS/Html

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ window.addEventListener('scroll', onScroll)
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
 function showNavOnScroll() {
@@ -21,7 +26,7 @@ function showBackToTopButtonOnScroll() {
     backToTopButton.classList.remove('show')
   }
 
-  backToTopButton.classList.toggle('inverseButton', scrollY > 4400)
+  backToTopButton.classList.toggle('inverseButton', scrollY > 4800)
 }
 
 function openMenu() {
@@ -29,6 +34,33 @@ function openMenu() {
 }
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+  // constante para determinar sua linha de visão na tela
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a Target Line?
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  // a base da seção chegou ou ultrapassou a Target Line?
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  //Trecho para selecionar a string do ID de cada Section
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 ScrollReveal({
